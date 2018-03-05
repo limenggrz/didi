a47c0ae5bf5fa70f24f2c6dda992906ca92cca14
小迪 <924524966@qq.com>
2018-03-05 16:45:38 +0800

    前台脚手架

---
+++ src/components/shop/Shop.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import appHeader from'./subcom/Header'
+    export default {
+        
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
